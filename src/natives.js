105d1ccac4d33347906479bc1017b1bc1a979eff
Andrey Belym <belym.a.2105@gmail.com>
2015-07-10 15:17:21 +0300

    Renamed shot-window to screenshot

--- src/natives.js
+++ src/natives.js
@@ -8,14 +8,14 @@ if (OS.win) {
     NATIVES = {
         findWindow:  toAbsPath('../bin/win/find-window.exe'),
         closeWindow: toAbsPath('../bin/win/close-window.exe'),
-        shotWindow:  toAbsPath('../bin/win/shot-window.exe')
+        shotWindow:  toAbsPath('../bin/win/screenshot.exe')
     };
 }
 else if (OS.mac) {
     NATIVES = {
         findWindow:  toAbsPath('../bin/mac/find-window.scpt'),
         closeWindow: toAbsPath('../bin/mac/close-window.scpt'),
-        shotWindow:  toAbsPath('../bin/mac/shot-window')
+        shotWindow:  toAbsPath('../bin/mac/screenshot')
     };
 }
 
