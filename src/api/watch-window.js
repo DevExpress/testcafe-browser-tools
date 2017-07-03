9124e46ece2ef424c1edcc7a5f920023150ab08f
Andrey Belym <belym.a.2105@gmail.com>
2017-07-03 17:09:06 +0300

    Implement browser window existence monitoring (#121)
    * Implement browser window existence monitoring
    
    * Add docs

---
+++ src/api/watch-window.js
@@ -0,0 +1,28 @@
+import findWindow from './find-window';
+import getWindowSize from './get-window-size';
+import delay from '../utils/delay';
+
+
+const DEFAULT_WATCHING_INTERVAL = 2000;
+
+/**
+ * Pause execution of a Promise chain while the specified browser window is opened.
+ * @function
+ * @name watchWindow
+ * @param {string} pageTitle - The title of the web page opened in the window that should be watched.
+ * @param {number} [watchingInterval=2000] - A time interval in milliseconds between checking the window state
+ * @returns Promise<undefined> A Promise which resolves when the browser window is closed.
+ **/
+export default async function (pageTitle, watchingInterval = DEFAULT_WATCHING_INTERVAL) {
+    var windowDescription = await findWindow(pageTitle);
+
+    if (!windowDescription)
+        return;
+
+    var windowSize = await getWindowSize(windowDescription);
+
+    while (windowSize && windowSize.width && windowSize.height) {
+        await delay(watchingInterval);
+        windowSize = await getWindowSize(windowDescription);
+    }
+}
