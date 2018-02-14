cc7bbd2cb62e91ab9812924ee135c4d6eba7b402
Andrey Churkin <alam17@mail.ru>
2018-02-14 14:07:38 +0300

    Implement bring-to-front function for MAC (#141)

--- src/api/bring-to-front.js
+++ src/api/bring-to-front.js
@@ -20,6 +20,8 @@ export default async function (windowDescriptor) {
 
     if (OS.win)
         bringWindowToFrontArguments = [windowDescription.hwnd];
+    else if (OS.mac)
+        bringWindowToFrontArguments = [windowDescription.windowId, windowDescription.bundleId];
     else
         return;
 
